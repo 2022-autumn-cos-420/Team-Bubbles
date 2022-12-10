322906d6aa3cc5f1dd69ff90b961325960a2a57b
Nate Roberts <naterob316@gmail.com>
2022-12-10 14:28:49 -0500

    readd tasks

--- timewielder/src/App.test.js
+++ timewielder/src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// test('background is dark gray', () => {
+//   render(<App />);
+//   const backgroundColor = screen.getByTestId(".app");
+//   expect(backgroundColor).toHaveStyle('background-color: #434343');
+// });
+
+test('sign in with Google button is rendered', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const signInWithGoogleButton = screen.getByText(/Sign in/);
+  expect(signInWithGoogleButton).toBeInTheDocument();
+});
+
+test('not logged in text is rendered', () => {
+  render(<App/>);
+  const notLoggedIn = screen.getByText(/Not Logged In/);
+  expect(notLoggedIn).toBeInTheDocument();
 });
